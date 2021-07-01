f377b2f8f52191c26b0b3e883bf175bf9beabc88
pasakon <kongpazam@gmail.com>
2021-07-01 20:30:14 +0700

    Mouse event and Add new button

--- main.js
+++ main.js
@@ -2,31 +2,31 @@
 const app = Vue.createApp({
     data() {
         return{
-            // product: 'Boots',
-            // Description: 'Is boots for cute people',
-            // image: './assets/images/socks_green.jpg',
+        
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
             inStock: true,
             Url:'https://www.camt.cmu.ac.th/index.php/th/',
+            image:'./assets/images/socks_green.jpg',
             inventory: 100,
             onSale: false,
             details:['50% cotton','30% wool','20% polyester'],
             variants:[
-                {id: 2234, color:'green'},
-                {id:2235, color:'blue'}
+                {id: 2234, color:'green',image: './assets/images/socks_green.jpg'},
+                {id:2235, color:'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes:['S','M','L'],
             cart: 0
         }
     },
     methods:{
-        Movetocamt(){
-            console.log('loveu')
-            window.location.href = 'https://www.camt.cmu.ac.th/index.php/th/'
-        },
         addToCart(){
           this.cart +=1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        Checkstatus(){
+            this.inStock =!this.inStock
         }
     }    
 })
