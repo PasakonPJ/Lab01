9a17abf6ff07a07b020b273eb0eb3e3d1c81e178
pasakon <kongpazam@gmail.com>
2021-07-01 19:56:32 +0700

    size of the sock in single line

--- main.js
+++ main.js
@@ -11,7 +11,12 @@ const app = Vue.createApp({
             Url:'https://www.camt.cmu.ac.th/index.php/th/',
             inventory: 100,
             onSale: false,
-            details:['50% cotton','30% wool','20% polyester']
+            details:['50% cotton','30% wool','20% polyester'],
+            variants:[
+                {id: 2234, color:'green'},
+                {id:2235, color:'blue'}
+            ],
+            sizes:['S','M','L'],
         }
     },
     methods:{
