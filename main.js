cf60f086553fb84f1ffd728e46ce8b12ad83ce22
pasakon <kongpazam@gmail.com>
2021-07-01 18:54:51 +0700

    add the dive which will show the output with image as given

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     data() {
         return{
             product: 'Boots',
-            Description: 'Is boots for cute people'
+            Description: 'Is boots for cute people',
+            image: './assets/images/socks_green.jpg'
         }
     }    
 })
