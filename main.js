93aa22b836687216e82fd5b9e3144fde0f4b9b2a
pasakon <kongpazam@gmail.com>
2021-07-01 19:20:04 +0700

    Add camt link

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const app = Vue.createApp({
         return{
             product: 'Boots',
             Description: 'Is boots for cute people',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_green.jpg',
+            Url:'https://www.camt.cmu.ac.th/index.php/th/'
+        }
+    },
+    methods:{
+        Movetocamt(){
+            console.log('loveu')
+            window.location.href = 'https://www.camt.cmu.ac.th/index.php/th/'
         }
     }    
 })
